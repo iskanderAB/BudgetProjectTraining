131838a48c64299ecf50bf6f840579270a4cc6c0
iskanderAB <iskanderabasi@gmail.com>
2021-02-24 01:22:54 +0100

    update UI budget and percentage

--- app.js
+++ app.js
@@ -17,17 +17,24 @@
             totals: {
                 exp: 0,
                 inc: 0
-            }
+            },
+            percentage : 100
         };
         return {
             addItem : (type, des, val) => {
+                // add new item 
                 let ID ;
                 data.allItems[type].length > 0 ? ID =  data.allItems[type][data.allItems[type].length-1].id  + 1 : ID = 0 ;
                 data.allItems[type].push(type === 'exp' ? new Expense(ID,des,val): new Income(ID, des, val)  );
-                console.log("add item return => " ,data.allItems[type][data.allItems[type].length-1]);
+                
+                // update total budget 
+                data.totals[type] += +val;
+                // uupdate percentage 
+                data.percentage =Math.round((data.totals.exp/(data.totals.inc - data.totals.exp))*100);
+                console.log("percentage => " ,  data.percentage);
                 return data.allItems[type][data.allItems[type].length-1];
             },
-            testing: ()=> console.log(data)
+            getData : () => data,
         }
     })();
 
@@ -38,15 +45,20 @@
             inputValue : '.add__value',
             inputBtn: '.add__btn',
             incomeContainer: '.income__list',
-            expenseContainer: '.expenses__list'
+            expenseContainer: '.expenses__list',
+            budgetValue: '.budget__value',
+            budgetInc: '.budget__income--value',
+            budgetExp: '.budget__expenses--value',
+            percentage: '.budget__expenses--percentage'
         };
+        
 
         return {
             getInput : () => {
                 return {
                     type: document.querySelector(DOMStrings.inputType).value,
                     description: document.querySelector(DOMStrings.inputDescription).value,
-                    value: document.querySelector(DOMStrings.inputValue).value
+                    value: +document.querySelector(DOMStrings.inputValue).value
                 }
             },
             addListItem : (obj) => {
@@ -81,6 +93,17 @@
                 console.log('element => ' , document.querySelector(element));
                 document.querySelector(element).insertAdjacentHTML('afterbegin',html);
             },
+            updateBudget : (totlaBudget=0, expBudget=0, incBudget=0,percentage=100) => {
+                let UITotalBudget = document.querySelector(DOMStrings.budgetValue);
+                let UIExpBudget = document.querySelector(DOMStrings.budgetExp);
+                let UIIncBudget = document.querySelector(DOMStrings.budgetInc);
+                let UIPercentage = document.querySelector(DOMStrings.percentage);
+                UITotalBudget.textContent= totlaBudget > 0  || totlaBudget === 0 ? '+' + totlaBudget : totlaBudget ;
+                UIExpBudget.textContent= '-' + expBudget ;
+                UIIncBudget.textContent=  '+' + incBudget ;
+                UIPercentage.textContent= percentage + ' %'; 
+                console.log(UITotalBudget);
+            } , 
             clearFields : () => {
                 // for return array not nodeList
                 const fields = [...document.querySelectorAll(DOMStrings.inputDescription+','+DOMStrings.inputValue)];
@@ -97,13 +120,25 @@
 
     controller =((budgetCtrl , UICtrl)=>{
         console.log("i'm running ");
+
+        const updateBudget = () => {
+            const totalBudget = budgetCtrl.getData().totals.inc - budgetCtrl.getData().totals.exp;
+            const incBudget = budgetCtrl.getData().totals.inc; 
+            const expBudget = budgetCtrl.getData().totals.exp; 
+            const pecentage = budgetCtrl.getData().percentage; 
+            console.log("updated budget => " , totalBudget);
+            UICtrl.updateBudget(totalBudget, expBudget, incBudget,pecentage);
+        };
         const addItem = () => {
             const inputs = UICtrl.getInput();
-            console.log("inputs => " , inputs);
-            console.log("add item ! ");
-            UICtrl.addListItem(budgetCtrl.addItem(inputs.type,inputs.description,inputs.value));
-            UICtrl.clearFields();
+            if( inputs.description !=='' && inputs.value > 0 && !isNaN(inputs.value) ){
+                UICtrl.addListItem(budgetCtrl.addItem(inputs.type,inputs.description,inputs.value));
+                UICtrl.clearFields();
 
+                // calculate adn update budget 
+                updateBudget();
+            }
+            
         };
         const setupEventListener = () => {
             const DOM = UICtrl.getDOMNames();
@@ -117,6 +152,7 @@
         return {
             init : () => {
                 console.log("Ctrl App running ");
+                UICtrl.updateBudget();
                 setupEventListener();
             }
         }
